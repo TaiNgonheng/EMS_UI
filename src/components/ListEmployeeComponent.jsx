5be633e33d8ceac4d52bed388aacc80e6099e8c6
TaiNgonheng <heng68807@gmail.com>
2024-10-31 20:50:08 +0700

    using navigator when adding end it will back to employee list, and also add useEffect for

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -1,42 +1,48 @@
 import React,{useEffect, useState} from 'react'
-import {listEmployes} from "../services/EmployeeService.jsx";
+import {listEmployees} from "../services/EmployeeService.jsx";
+import {useNavigate} from "react-router-dom";
 
 const ListEmployeeComponent = () => {
 
     const [employees, setEmployee] = useState([])
+    const navigator = useNavigate();
 
-    useEffect(()=>{
-       listEmployes().then((response)=>{
-           setEmployee(response.data);
-       }).catch(error=>{
-           console.error(error);
-       })
+    useEffect(()=>{listEmployees().then((response)=>
+    {setEmployee(response.data);
+    }).catch(error=>{
+        console.error(error);
+    })
     }, [])
+
+    function addNewEmployee(){
+        navigator('/addEmployee')
+    }
 
 
     return (
         <div className="container">
             <h2 className="text-center">List of Employee</h2>
+            <button className="btn btn-primary" onClick={addNewEmployee}>Add Employee</button>
             <table className="table table-striped table-bordered">
                 <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                    </tr>
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                </tr>
                 </thead>
-                    <tbody>
-                    {
-                        employees.map((employee) =>
+                <tbody>
+                {
+                    employees.map((employee) =>
                         <tr key={employee.id}>
                             <td>{employee.id}</td>
                             <td>{employee.firstname}</td>
                             <td>{employee.lastname}</td>
                             <td>{employee.email}</td>
                         </tr>)
-                     }
-                    </tbody>
+                }
+                </tbody>
             </table>
         </div>
     )
