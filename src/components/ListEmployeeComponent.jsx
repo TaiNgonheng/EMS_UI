a7e380bad896ac2ca69e29cebf1717bdf6282d14
TaiNgonheng <heng68807@gmail.com>
2024-11-02 11:48:12 +0700

    using useEffect for response data and catch error use navigator to connect page

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -1,5 +1,5 @@
 import React,{useEffect, useState} from 'react'
-import {listEmployees} from "../services/EmployeeService.jsx";
+import {deleteEmployee, listEmployees} from "../services/EmployeeService.jsx";
 import {useNavigate} from "react-router-dom";
 
 const ListEmployeeComponent = () => {
@@ -7,22 +7,36 @@ const ListEmployeeComponent = () => {
     const [employees, setEmployee] = useState([])
     const navigator = useNavigate();
 
-    useEffect(()=>{listEmployees().then((response)=>
-    {setEmployee(response.data);
-    }).catch(error=>{
-        console.error(error);
-    })
+    useEffect(()=>{
+        getAllEmployees();
     }, [])
 
+    function getAllEmployees(){
+        listEmployees().then((response)=>
+        {setEmployee(response.data);
+        }).catch(error=>{
+            console.error(error);
+        })
+    }
     function addNewEmployee(){
         navigator('/addEmployee')
     }
-
+    function updateEmployee(id){
+        navigator(`/editEmployee/${id}`)
+    }
+    function removeEmployee(id){
+        console.log(id);
+        deleteEmployee(id).then((response)=>{
+            getAllEmployees();
+        }).catch((error)=>{
+            console.error(error);
+        })
+    }
 
     return (
         <div className="container">
             <h2 className="text-center">List of Employee</h2>
-            <button className="btn btn-primary" onClick={addNewEmployee}>Add Employee</button>
+            <button className="btn btn-primary mb-2" onClick={addNewEmployee}>Add Employee</button>
             <table className="table table-striped table-bordered">
                 <thead>
                 <tr>
@@ -30,6 +44,7 @@ const ListEmployeeComponent = () => {
                     <th>First Name</th>
                     <th>Last Name</th>
                     <th>Email</th>
+                    <th>Actions</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -40,6 +55,10 @@ const ListEmployeeComponent = () => {
                             <td>{employee.firstname}</td>
                             <td>{employee.lastname}</td>
                             <td>{employee.email}</td>
+                            <td>
+                            <button className='btn btn-info' onClick={ () => updateEmployee(employee.id)}>Update</button>
+                            <button className='btn btn-danger ms-3' onClick={()=>removeEmployee(employee.id)}>Delete</button>
+                            </td>
                         </tr>)
                 }
                 </tbody>
