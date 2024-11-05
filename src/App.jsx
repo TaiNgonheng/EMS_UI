76e17e660c79069ab1db3194521df1e134471433
TaiNgonheng <heng68807@gmail.com>
2024-11-05 21:50:27 +0700

    Add Route to Department section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import FooterComponent from "./components/FooterComponent.jsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './App.css'
 import EmployeeComponent from "./components/EmployeeComponent.jsx";
+import ListDepartmentComponent from "./components/ListDepartmentComponent.jsx";
+import DepartmentComponent from "./components/DepartmentComponent.jsx";
 export default function App(){
     return (
         <>
@@ -14,6 +16,9 @@ export default function App(){
                     <Route path='/employee' element={<ListEmployeeComponent/>}></Route>
                     <Route path='/addEmployee' element={<EmployeeComponent/>}></Route>
                     <Route path='/editEmployee/:id' element={<EmployeeComponent/>}></Route>
+                    <Route path='/department' element={<ListDepartmentComponent/>}></Route>
+                    <Route path='/add-department' element={<DepartmentComponent/>}></Route>
+                    <Route path='/edit-department/:id' element={<DepartmentComponent/>}></Route>
                 </Routes>
                 <FooterComponent/>
             </BrowserRouter>
